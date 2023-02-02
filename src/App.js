dc46e12d6228aad174a49a22c33371773255dcb2
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-02 17:33:37 +0000

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { AppBar, Button, Select, TextField,MenuItem } from '@mui/material';
+import axios from 'axios';
 function App(){
   
-  
+  axios.get('http://127.0.0.1:5000/').then((res) =>{
+    console.log(res);
+  })
+
   return (
     
     <div className="App">
       <AppBar position='static' id='AppBar'>LINGUIST</AppBar>
       <header className="App-header">
-        <div>
-          <Select>
+        <div id="selects">
+
+          <Select >
             <MenuItem value={'en'}>English</MenuItem>
             <MenuItem value={'fr'}>French</MenuItem>
             <MenuItem value={'es'}>Spanish</MenuItem>
           </Select>
           <br/>
-          <Select>
+          <Select >
             <MenuItem value={'en'}>English</MenuItem>
             <MenuItem value={'fr'}>French</MenuItem>
             <MenuItem value={'es'}>Spanish</MenuItem>
