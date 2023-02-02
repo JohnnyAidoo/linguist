c4ece8a93399d9bd215bfeebcbac6e0ea68635ff
Johnny Osei Aidoo <johnnyoseiaidoo65@gmail.com>
2023-02-02 17:33:16 +0000

    'commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { useEffect } from 'react';
 import './App.css';
-import { AppBar, Button, TextField } from '@mui/material';
+import { AppBar, Button, Select, TextField,MenuItem } from '@mui/material';
 function App(){
+  
+  
   return (
+    
     <div className="App">
       <AppBar position='static' id='AppBar'>LINGUIST</AppBar>
       <header className="App-header">
+        <div>
+          <Select>
+            <MenuItem value={'en'}>English</MenuItem>
+            <MenuItem value={'fr'}>French</MenuItem>
+            <MenuItem value={'es'}>Spanish</MenuItem>
+          </Select>
+          <br/>
+          <Select>
+            <MenuItem value={'en'}>English</MenuItem>
+            <MenuItem value={'fr'}>French</MenuItem>
+            <MenuItem value={'es'}>Spanish</MenuItem>
+          </Select>
+        </div>
         <TextField multiline label='input here' id='fullWidth'></TextField>
         <Button variant='contained' id='button'>translate</Button>
         <TextField multiline id='fullWidth'> translated </TextField>
